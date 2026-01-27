a1b1825a374271b76f1fcd4cb115121e8e067ad5
Monir <hossinmonir681@gmail.com>
2026-01-27 10:24:42 +0600

    redux ititial setup done

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./featurs/auth/authSlice"
 
 export const store = configureStore({
-    reducer:{}
+    reducer:{
+        auth:authReducer,
+    }
 })
 
 
